1118ce4af9d94c920ae14af8f2d388eb82d7b068
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-05-20 16:01:34 +0530

    Fixed the arrow function

--- src/head.js
+++ src/head.js
@@ -1,9 +1,7 @@
 const splitLines = (lines) => lines.split('\n');
 const joinLines = (lines) => lines.join('\n');
 
-const linesUpTo = (lines, linesCount) => {
-  return lines.slice(0, linesCount);
-};
+const linesUpTo = (lines, linesCount) => lines.slice(0, linesCount);
 
 const head = (lines, { linesCount }) => {
   const splittedLines = splitLines(lines);

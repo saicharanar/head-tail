383a808d3278d974467b7c988338b2394e8bd15c
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-05-25 13:00:45 +0530

    Added try block in tail.js

--- tail.js
+++ tail.js
@@ -1,5 +1,9 @@
 const { tailMain } = require('./src/tail/tailLib');
 const fs = require('fs');
 
-console.log(tailMain(fs.readFileSync, process.argv.slice(2)));
+try {
+  console.log(tailMain(fs.readFileSync, process.argv.slice(2)));
+} catch (error) {
+  console.error(`tail: ${error.message}`);
+}
 
